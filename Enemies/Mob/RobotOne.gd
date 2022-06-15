d6ad2c6dfc6a5d477e76f53a64193d6c1ed8c563
Lucas Lacerda <lucasalvlacerda@gmail.com>
2022-06-15 00:15:55 -0300

    adiciona efeitos genéricos conectando com sinais direto no código e não mais de forma manual; adiciona efeito de morte do inimigo

--- Enemies/Mob/RobotOne.gd
+++ Enemies/Mob/RobotOne.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
 var knockback = Vector2.ZERO
 
 onready var stats = $Stats
@@ -14,3 +16,6 @@ func _on_Hurtbox_area_entered(area):
 
 func _on_Stats_no_health():
 	queue_free()
+	var enemy_death_effect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemy_death_effect)
+	enemy_death_effect.global_position = global_position
--- World/Objects/WoodBox.gd
+++ World/Objects/WoodBox.gd
@@ -1,14 +1,12 @@
 extends StaticBody2D
 
+const WoodBoxEffect = preload("res://Effects/WoodBoxEffect.tscn")
+
+func create_grass_effect():
+	var box_effect = WoodBoxEffect.instance()
+	get_parent().add_child(box_effect)
+	box_effect.global_position = global_position
+
 func _on_Hurtbox_area_entered(area):
 	create_grass_effect()
 	queue_free()
-
-func create_grass_effect():
-	var WoodBoxEffect = load("res://Effects/WoodBoxEffect.tscn")
-	var box_effect = WoodBoxEffect.instance()
-		
-	var world = get_tree().current_scene
-	world.add_child(box_effect)
-	box_effect.global_position = global_position
-
