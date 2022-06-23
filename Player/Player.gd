464fc022db970057fa26c1d5969671e427d1cad3
Lucas Lacerda <lucasalvlacerda@gmail.com>
2022-06-23 01:11:04 -0300

    player stats

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,13 +13,16 @@ enum {
 var state = MOVE
 var velocity = Vector2.ZERO
 var roll_vector = Vector2.LEFT
+var stats = PlayerStats
 
 onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
 onready var sword_hitbox = $HitboxPivot/SwordHitbox
+onready var hurtbox = $Hurtbox
 
 func _ready():
+	stats.connect("no_health", self, "queue_free")
 	animation_tree.active = true
 	sword_hitbox.knockback_vector = roll_vector
 
@@ -62,3 +65,10 @@ func attack_state(delta):
 
 func attack_animation_finished():
 	state = MOVE
+
+func _on_Hurtbox_area_entered(area):
+	print("1111")
+	print(stats.health)
+	hurtbox.start_invincibility(0.5)
+	hurtbox.create_hit_effect()
+	stats.health -= 1
